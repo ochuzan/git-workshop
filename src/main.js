2861b63faff8a07ef41af5d69747b7db8a9d667d
Oliver Chuzan <oliverchuzan@Olivers-MacBook-Pro.local>
2023-03-07 11:04:31 -0500

    Add utility function

---
+++ src/main.js
@@ -0,0 +1,14 @@
+function addNumbers(input) {
+    let sum = 0;
+    for (let i = 0; i < input.length; i++) {
+        sum += input;
+    }
+
+    return sum;
+}
+
+function main (){
+    addNumbers(1,2,3)
+}
+
+main()
